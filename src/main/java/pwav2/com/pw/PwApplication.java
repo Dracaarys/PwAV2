610c626d254d25a511ee4b92bf2924b80f6c9b31
Dracaarys <adlergabriel558@gmail.com>
2024-07-25 19:46:09 -0300

    iniciala

--- src/main/java/pwav2/com/pw/PwApplication.java
+++ src/main/java/pwav2/com/pw/PwApplication.java
@@ -2,7 +2,7 @@ package pwav2.com.pw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//a
 @SpringBootApplication
 public class PwApplication {
 
